be7169faea765bc9229091d6a82b45263bf55d31
Ariel Rodrigues <ariel.dev22@gmail.com>
2024-09-28 19:05:46 -0300

    feat: configuração do endpoint DELETE de users

--- src/main/java/com/arieldev22/auth_app/auth/security/SecurityConfig.java
+++ src/main/java/com/arieldev22/auth_app/auth/security/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/users/delete/{id}").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
