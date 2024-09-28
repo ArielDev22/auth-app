02100a97ddc842ca0570255a882714f450420e40
Ariel Rodrigues <ariel.dev22@gmail.com>
2024-09-28 15:03:11 -0300

    feat: adição do método findByEmail em UserRepository

--- src/main/java/com/arieldev22/auth_app/user/repository/UserRepository.java
+++ src/main/java/com/arieldev22/auth_app/user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.arieldev22.auth_app.user.repository;
 
 import com.arieldev22.auth_app.user.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByEmail(String email);
 }
