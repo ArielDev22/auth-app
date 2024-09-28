75485a0c197e46a4dcbc74c2ded2ddbd0a97be44
Ariel Rodrigues <ariel.dev22@gmail.com>
2024-09-28 16:40:29 -0300

    feat: configuração de autorização das requests

--- src/main/java/com/arieldev22/auth_app/auth/security/SecurityConfig.java
+++ src/main/java/com/arieldev22/auth_app/auth/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.arieldev22.auth_app.auth.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,6 +26,11 @@ public class SecurityConfig {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .anyRequest().authenticated()
+                )
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
