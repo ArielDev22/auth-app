813b9492c717e27755e6f8a6713f02167b3b7205
Ariel Rodrigues <ariel.dev22@gmail.com>
2024-09-28 19:06:20 -0300

    fix: fix na criação das litas de roles

--- src/main/java/com/arieldev22/auth_app/user/models/User.java
+++ src/main/java/com/arieldev22/auth_app/user/models/User.java
@@ -43,8 +43,8 @@ public class User implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         if (this.role == UserRole.ADMIN)
-            return List.of(new SimpleGrantedAuthority("ADMIN_ROLE"), new SimpleGrantedAuthority("USER_ROLE"));
-        else return List.of(new SimpleGrantedAuthority("USER_ROLE"));
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     @Override
