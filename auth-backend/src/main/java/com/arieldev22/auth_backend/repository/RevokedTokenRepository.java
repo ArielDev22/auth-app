f4761f50bf41b52a1a3ea0691db9023fb70c6001
Ariel Rodrigues <ariel.dev22@gmail.com>
2024-11-16 13:03:35 -0300

    feat(backend): adiciona a função finByToken em RevokedTokenRepository
    A função procura por um token revogado no banco de dados

--- auth-backend/src/main/java/com/arieldev22/auth_backend/repository/RevokedTokenRepository.java
+++ auth-backend/src/main/java/com/arieldev22/auth_backend/repository/RevokedTokenRepository.java
@@ -3,5 +3,8 @@ package com.arieldev22.auth_backend.repository;
 import com.arieldev22.auth_backend.model.RevokedToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RevokedTokenRepository extends JpaRepository<RevokedToken, Long> {
+    Optional<RevokedToken> findByToken(String token);
 }
