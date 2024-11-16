06d876025f1cdba3637f7abb48ba0218184264a0
Ariel Rodrigues <ariel.dev22@gmail.com>
2024-11-16 15:04:52 -0300

    feat(backend): cria a exceção para credenciais inválidas

---
+++ auth-backend/src/main/java/com/arieldev22/auth_backend/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package com.arieldev22.auth_backend.exceptions;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
