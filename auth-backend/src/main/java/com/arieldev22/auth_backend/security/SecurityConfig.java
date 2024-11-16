0d4371aaaba98b3b0c4c24919c9b0ae181f0ceac
Ariel Rodrigues <ariel.dev22@gmail.com>
2024-11-16 15:13:43 -0300

    feat(backend): adiciona a permissão de acesso a url /auth/
    a configuração se aplica as requisições post e o acesso é permitido para qualquer usuario

--- auth-backend/src/main/java/com/arieldev22/auth_backend/security/SecurityConfig.java
+++ auth-backend/src/main/java/com/arieldev22/auth_backend/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.arieldev22.auth_backend.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,6 +30,7 @@ public class SecurityConfig {
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                 )
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
