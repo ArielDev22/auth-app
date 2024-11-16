60bce4beba8995317c0a5e1958bd1a5446e00358
Ariel Rodrigues <ariel.dev22@gmail.com>
2024-11-15 23:37:42 -0300

    feat(backend): adiciona função findByEmail
    A função procura no banco de dados um usuário que contém o email passado como parâmetro

--- auth-backend/src/main/java/com/arieldev22/auth_backend/repository/UserRepository.java
+++ auth-backend/src/main/java/com/arieldev22/auth_backend/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.arieldev22.auth_backend.repository;
 
 import com.arieldev22.auth_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByEmail(String email);
 }
