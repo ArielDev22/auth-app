0eef9de795bee9b92575114b4746b5927c09e3a7
Ariel Rodrigues <ariel.dev22@gmail.com>
2024-11-16 21:05:51 -0300

    feat(frontend): configura login e home routes

--- auth-frontend/src/App.jsx
+++ auth-frontend/src/App.jsx
@@ -6,6 +6,8 @@ import {
   Navigate,
 } from "react-router-dom";
 import RegisterPage from "./pages/register/registerPage";
+import LoginPage from "./pages/login/LoginPage";
+import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/" element={<Navigate to="/auth/register" />} />
 
         <Route path="/auth/register" element={<RegisterPage />} />
+        <Route path="/auth/login" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </Router>
   );
